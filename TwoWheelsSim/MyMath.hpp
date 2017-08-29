8a15fa1ecc534426f728d1e06e2e3bb8e5ed3453
migiright <migigit@yahoo.co.jp>
2017-08-29 18:35:06 +0900

    最初のコミット

---
+++ TwoWheelsSim/MyMath.hpp
@@ -0,0 +1,211 @@
+﻿#pragma once
+
+#include <array>
+#include <boost/format.hpp>
+#include <boost/operators.hpp>
+#include <boost/range.hpp>
+
+/**
+* @brief 行列
+* @tparam H 行数
+* @tparam W 列数
+*/
+template<size_t H, size_t W>
+class Matrix
+	: boost::multipliable<Matrix<H, W>>
+{
+public:
+	constexpr static size_t Height = H; ///< 行数
+	constexpr static size_t Width = W; ///< 列数
+
+	std::array<double, Height*Width> v;
+
+	/// 0で初期化する
+	Matrix()
+		: v{}
+	{}
+
+	/// コピーコンストラクタ
+	Matrix(const Matrix<H, W> &o)
+		: v(o.v)
+	{}
+
+	/**
+	* \brief 初期値を指定する
+	* \param args 初期化する値。1行1列, ... , 1行Width列, 2行1列, ... の順
+	*/
+	template<class ...Args>
+	explicit Matrix(const Args &...args)
+		: v{args...}
+	{}
+
+	/// 要素を返す
+	double& operator()(size_t row, size_t column)
+	{
+		return v[row * Width + column];
+	}
+
+	/// 要素を返す
+	double operator()(size_t row, size_t column) const
+	{
+		return v[row * Width + column];
+	}
+
+	constexpr static double height() { return Height; } ///< 行数を返す
+	constexpr static double width() { return Width; } ///< 列数を返す
+
+													  /// 整形して文字列として返す
+	std::string toString() const
+	{
+		std::string s;
+		for (size_t r = 0; r < Height; ++r) {
+			for (size_t c = 0; c < Width; ++c) {
+				s += (boost::format("%4.2f") % (*this)(r, c)).str() + " ";
+			}
+			s += "\n";
+		}
+		return s;
+	}
+
+	/// スカラとの乗算
+	Matrix<Height, Width>& operator*=(double rhs)
+	{
+		for (size_t r = 0; r < Height; ++r) {
+			for (size_t c = 0; c < Width; ++c) {
+				(*this)(r, c) *= rhs;
+			}
+		}
+		return *this;
+	}
+
+};
+
+/// 行列同士の乗算
+template<size_t LhsWidth, size_t AnsHright, size_t AnsWidth>
+Matrix<AnsHright, AnsWidth> operator*(
+	const Matrix<AnsHright, LhsWidth>& lhs, const Matrix<LhsWidth, AnsWidth>& rhs)
+{
+	Matrix<AnsHright, AnsWidth> ret;
+	for (size_t r = 0; r < AnsHright; ++r) {
+		for (size_t c = 0; c < AnsWidth; ++c) {
+			double s = 0;
+			for (size_t i = 0; i < LhsWidth; ++i) {
+				s += lhs(r, i) * rhs(i, c);
+			}
+			ret(r, c) = s;
+		}
+	}
+	return ret;
+}
+
+
+/**
+* \brief ベクトル
+* \tparam D 次元
+*/
+template<size_t D>
+struct Vector
+	: boost::addable<Vector<D>>
+	, boost::subtractable<Vector<D>>
+	, boost::multipliable<Vector<D>, double>
+	, boost::dividable<Vector<D>, double>
+{
+	std::array<double, D> v;
+	constexpr static size_t Dimension = D;
+
+	/// 0で初期化する
+	Vector() : v{}
+	{}
+
+	/// コピーコンストラクタ
+	Vector(const Vector &o)
+		: v(o.v)
+	{}
+
+	/// 初期値を指定する
+	template<class ...Args>
+	explicit Vector(const Args &...args)
+		: v{args...}
+	{}
+
+	/// 要素を返す
+	double& operator[](unsigned index) { return v[index]; }
+
+	/// 要素を返す
+	const double& operator[](unsigned index) const { return v[index]; }
+
+	/// 加算
+	Vector<D>& operator+=(const Vector<D>& rhs)
+	{
+		for (size_t i = 0; i < D; i++) {
+			v[i] += rhs[i];
+		}
+		return *this;
+	}
+
+	/// 減算
+	Vector<D>& operator-=(const Vector<D>& rhs)
+	{
+		for (size_t i = 0; i < D; i++) {
+			v[i] -= rhs[i];
+		}
+		return *this;
+	}
+
+	/// スカラとの乗算
+	Vector<D>& operator*=(double rhs)
+	{
+		for (size_t i = 0; i < D; ++i) {
+			(*this)[i] *= rhs;
+		}
+		return *this;
+	}
+
+	/// スカラでの除算
+	Vector<D>& operator/=(double rhs)
+	{
+		for (size_t i = 0; i < D; ++i) {
+			(*this)[i] /= rhs;
+		}
+		return *this;
+	}
+
+	/// 整形して文字列として返す
+	std::string toString()
+	{
+		std::string s;
+		for (size_t i = 0; i < D; ++i) {
+			s += (boost::format("%4.2f") % (*this)[i]).str() + " ";
+		}
+		s += "\n";
+		return s;
+	}
+};
+
+/// 行列*縦ベクトルの乗算
+template<size_t LhsHeight, size_t LhsWidth>
+Vector<LhsHeight> operator*(Matrix<LhsHeight, LhsWidth> lhs, Vector<LhsWidth> rhs)
+{
+	Vector<LhsHeight> ret;
+	for (size_t i = 0; i < LhsHeight; i++) {
+		ret[i] = 0;
+		for (size_t j = 0; j < LhsWidth; j++) {
+			ret[i] += lhs(i, j) * rhs[j];
+		}
+	}
+	return ret;
+}
+
+/// 横ベクトル*行列の乗算
+template<size_t LhsHeight, size_t LhsWidth>
+Vector<LhsWidth> operator*(Vector<LhsHeight> rhs, Matrix<LhsHeight, LhsWidth> lhs)
+{
+	Vector<LhsWitdh> ret;
+	for (size_t i = 0; i < LhsWidth; i++) {
+		ret[i] = 0;
+		for (size_t j = 0; j < LhsHeight; j++) {
+			ret[i] += rhs[j] * lhs(j, i);
+		}
+	}
+	return ret;
+}
---
+++ TwoWheelsSim/main.cpp
@@ -0,0 +1,155 @@
+﻿#include "DxLib.h"
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include "MyMath.hpp"
+
+using namespace std;
+
+DINPUT_JOYSTATE joyState;
+
+int joypadInputState; //押されているボタン
+int joypadPushedState; //押されていない状態から押されている状態になったボタン
+constexpr double Fps = 60; //1秒あたりのフレーム数
+constexpr double Cycle = 1.0 / 60000; //微小時間
+double c1 = 200, c2 = 120; //q[0]とq[1]の制約
+bool virtualEnabled = true; //コントローラーの入力を仮想入力に入れるか直接実際の入力に入れるか
+Vector<3> q; //元のシステムの状態
+Vector<3> xi2; //
+
+//元のシステムのg(q)
+Matrix<3, 2> gq(const Vector<3> &q)
+{
+	Matrix<3, 2> ret;
+	ret(0, 0) = cos(q[2]);
+	ret(0, 1) = 0;
+	ret(1, 0) = sin(q[2]);
+	ret(1, 1) = 0;
+	ret(2, 0) = 0;
+	ret(2, 1) = 1;
+	return ret;
+}
+
+//チェインドシステムの状態に変換
+Vector<3> convertToChained(const Vector<3> q)
+{
+	return Vector<3>{q[0], tan(q[2]), q[1]};
+}
+
+//チェインドシステムの状態をシステム蘇生変換後のチェインドシステムの状態に変換
+Vector<3> phi(const Vector<3> x)
+{
+	Vector<3> xi;
+	xi[0] = tan(M_PI * x[0] / (2 * c1));
+	xi[2] = tan(M_PI * x[2] / (2 * c2));
+	xi[1] = c1 * (xi[2] * xi[2] + 1) * x[1] / (c2 * (xi[0] * xi[0] + 1));
+	return xi;
+}
+
+//チェインドシステムの状態を元の状態に変換
+Vector<3> convertFeomChained(const Vector<3> xi)
+{
+	Vector<3> th;
+	th[0] = xi[0]; //システム蘇生変換後のチェインドシステムからシステム蘇生変換後のもとのシステムへ
+	th[1] = xi[2];
+	th[2] = atan(xi[1]) + (M_PI / 2 < q[2] && q[2] < 3 * M_PI / 2 ? M_PI : 0);
+	return th;
+}
+
+void resetSystem()
+{
+	q[0] = q[1] = q[2] = 0;
+	xi2[0] = xi2[1] = xi2[2] = 0;
+}
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	// 画面モードのセット
+	SetGraphMode(640, 480, 16);
+	ChangeWindowMode(TRUE);
+	if(DxLib_Init() == -1) return -1;
+	// 描画先画面を裏画面にセット
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	Vector<3> v{1.0, 2.0, 3.0};
+
+	// ループ
+	while(ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0) {
+		clsDx(); //画面消去
+
+		// コントローラ入力取得
+		GetJoypadDirectInputState(DX_INPUT_PAD1, &joyState);
+		int pjs = joypadInputState;
+		joypadInputState = GetJoypadInputState(DX_INPUT_PAD1);
+		//前フレームの入力(pjs)が0、かつ今回の入力(joypadInputState)が1のとき、1
+		joypadPushedState = ~pjs & joypadInputState;
+
+		//Aボタンで仮想入力と実際の入力の切り替え
+		if(joypadPushedState & PAD_INPUT_1) virtualEnabled = !virtualEnabled;
+		//Xボタンでリセット
+		if(joypadPushedState & PAD_INPUT_3) resetSystem();
+
+		Vector<3> th; //システム蘇生変換後のもとのシステム
+		Vector<2> nu, mu;
+		for(int i = 0; i < 1 / Cycle / Fps; ++i) { //1フレームで1/60
+			//+入力を決める
+			//状態変数を変換
+			Vector<3> x(convertToChained(q)); //元のシステムからチェインドシステムへ
+			Vector<3> xi(phi(x));
+			th[0] = xi[0]; //システム蘇生変換後のチェインドシステムからシステム蘇生変換後のもとのシステムへ
+			th[1] = xi[2];
+			th[2] = atan(xi[1]) + (M_PI / 2 < q[2] && q[2] < 3 * M_PI / 2 ? M_PI : 0);
+			//入力をpadから求めて入力変換
+			if(virtualEnabled) {
+				nu[0] = static_cast<double>(joyState.Y) * -0.005; //システム蘇生変換後の元のシステムの仮想入力
+				nu[1] = static_cast<double>(joyState.Rx) * 0.004;
+				Vector<2> v{ //システム蘇生変換後の元のシステムの仮想入力からシステム蘇生変換後のチェインドシステムの仮想入力へ
+					nu[0] * cos(th[2]),
+					nu[1] / (cos(th[2]) * cos(th[2])),
+				};
+				Vector<2> u{//システム蘇生変換後のチェインドシステムの仮想入力からチェインドシステムの入力へ
+					2 * c1 * v[0] / (M_PI * (1 + xi[0] * xi[0])),
+					c2 * (2 * xi[1] * v[0] * (xi[0] - xi[1] * xi[2] * (xi[0] * xi[0] + 1)) + v[1] * (xi[0] * xi[0] + 1)) / (c1 * (xi[2] * xi[2] + 1)),
+				};
+				mu[0] = u[0] / cos(q[2]);
+				mu[1] = u[1] * cos(q[2]) * cos(q[2]);
+			} else {
+				mu[0] = static_cast<double>(joyState.Y) * -0.3; //元のシステムの入力
+				mu[1] = static_cast<double>(joyState.Rx) * 0.004;
+			}
+			//-入力を決める
+			//状態を更新
+			q += Cycle * (gq(q) * mu);
+			q[2] = fmod(M_PI * 2 + fmod(q[2], M_PI * 2), M_PI * 2); //角度を0~2πの範囲にする
+		}
+
+		// 画面を初期化する
+		ClearDrawScreen();
+		// プレイヤーを描画する
+		printfDx("仮想入力:%s 切り替えはAボタン\n", virtualEnabled ? "ON" : "OFF");
+		printfDx("リセットはXボタン\n");
+		printfDx("q  = { %4.6f, %4.6f, %4.6f }\n", q[0], q[1], q[2]);
+		printfDx("mu = { %4.6f, %4.6f }\n", mu[0], mu[1]);
+		printfDx("th = { %4.6f, %4.6f, %4.6f }\n", th[0], th[1], th[2]);
+		printfDx("nu = { %4.6f, %4.6f }\n", nu[0], nu[1]);
+		DrawCircle(static_cast<int>(th[0] + 320), static_cast<int>(th[1] + 240), 8, GetColor(64, 64, 128));
+		DrawLine(static_cast<int>(th[0] + 320), static_cast<int>(th[1] + 240),
+			static_cast<int>(320 + th[0] + 16 * cos(th[2])), static_cast<int>(240 + th[1] + 16 * sin(th[2])),
+			GetColor(128, 128, 0));
+		DrawCircle(static_cast<int>(320 + q[0]), static_cast<int>(240 + q[1]), 8, GetColor(128, 128, 255));
+		DrawLine(static_cast<int>(320 + q[0]), static_cast<int>(240 + q[1]),
+			static_cast<int>(320 + q[0] + 16 * cos(q[2])), static_cast<int>(240 + q[1] + 16 * sin(q[2])),
+			GetColor(255, 255, 0));
+		//枠
+		DrawBox(static_cast<int>(320 - c1), static_cast<int>(240 - c2),
+			static_cast<int>(320 + c1), static_cast<int>(240 + c2), GetColor(255, 0, 0), FALSE);
+		// 裏画面の内容を表画面に反映させる
+		ScreenFlip();
+	}
+	DxLib_End(); // ＤＸライブラリ使用の終了処理
+
+	return 0; // ソフトの終了
+}
+
+void f()
+{}
